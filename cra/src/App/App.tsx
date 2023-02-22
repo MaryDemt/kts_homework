3b993c35d4556fcd0b80588bb699ce931c03f200
mshevtsova <mary.shevtcova@yandex.ru>
2023-02-22 15:46:33 +0300

    add routes for products and one product

--- cra/src/App/App.tsx
+++ cra/src/App/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "../styles/styles.scss";
+import Product from "./pages/Product";
+import Products from "./pages/Products";
 
 function App() {
-  return <div className="app">create-react-app</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/:id" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
