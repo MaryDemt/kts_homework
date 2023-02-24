b5230c740652cef4748263de9e98a26d759852de
mshevtsova <mary.shevtcova@yandex.ru>
2023-02-25 00:03:37 +0300

    add infinity scroll

--- cra/src/App/pages/Products/Products.tsx
+++ cra/src/App/pages/Products/Products.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 
 import axios, { AxiosResponse } from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-import Card from "./components/Card";
-
+import CardItem from "./components/Card";
 import "./Products.scss";
+import { FilterIcon } from "../../components/icons/filter_icon";
+import { SearchIcon } from "../../components/icons/search_icon";
 
 export interface ProductItem {
   id: number;
@@ -25,6 +27,9 @@ export interface ProductItem {
 
 const Products = () => {
   const [listOfProducts, setListOfProducts] = useState<ProductItem[]>([]);
+  const [listLength, setListLength] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [pagesMax, setPagesMax] = useState<number>(0);
 
   useEffect(() => {
     handleGetDataProducts();
@@ -33,20 +38,62 @@ const Products = () => {
   const handleGetDataProducts = async () => {
     let responseData: AxiosResponse = await axios({
       method: "get",
-      url: "https://api.escuelajs.co/api/v1/products?offset=0&limit=12",
+      url: "https://api.escuelajs.co/api/v1/products",
     });
-    setListOfProducts(responseData.data);
+    setListLength(responseData.data.length);
+    setPagesMax(Math.floor(responseData.data.length / 12));
+    setListOfProducts(responseData.data.slice(0, 12));
   };
+
+  const handleAddNewProducts = async () => {
+    let responseData: any = await axios({
+      method: "get",
+      url: `https://api.escuelajs.co/api/v1/products?offset=${
+        currentPage + 1
+      }&limit=12`,
+    });
+    setListOfProducts((prev) => [...prev, ...responseData.data]);
+    setCurrentPage((prev) => prev + 1);
+  };
+
   return (
     <>
-      <h1 className="products__title">Total Product</h1>
+      <h1 className="products__title">Products</h1>
+      <p className="products__description">
+        We display products based on the latest products we have, if you want to
+        see our old products please enter the name of the item
+      </p>
+      <div className="products__search">
+        <label className="products__label">
+          <SearchIcon />
+          <input className="products__input" placeholder="Search property" />
+          <button className="products__search-button">Find Now</button>
+        </label>
+        <button className="products__button">
+          <FilterIcon />
+          Filter
+        </button>
+      </div>
+      <h2 className="products__subtitle">
+        Total Product <span className="products__length">{listLength}</span>
+      </h2>
       <ul className="products__list">
         {listOfProducts.length
           ? listOfProducts.map((it) => {
-              return <Card {...it} />;
+              return <CardItem {...it} />;
             })
           : ""}
       </ul>
+      <InfiniteScroll
+        dataLength={listOfProducts.length}
+        next={handleAddNewProducts}
+        hasMore={true}
+        loader={<h4>Loading...</h4>}
+      >
+        {listOfProducts.map((i, index) => (
+          <div key={index}>div - #{index}</div>
+        ))}
+      </InfiniteScroll>
     </>
   );
 };
--- cra/src/App/pages/Products/components/Card/Card.tsx
+++ cra/src/App/pages/Products/components/Card/Card.tsx
@@ -1,16 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
 import { ProductItem } from "../../../Products/Products";
 
 import "./Card.scss";
 
-const Card = (item: ProductItem) => {
+const CardItem = (item: ProductItem) => {
+  const navigate = useNavigate();
+
+  const handleClick = (id: number) => {
+    navigate(`/${id}`);
+  };
   return (
-    <li key={item.id} className="card">
+    <li key={item.id} className="card" onClick={() => handleClick(item.id)}>
       <img
         className="card__image"
         src={item.images && item.images.length ? item.images[0] : ""}
         alt={item.description}
       />
-      <p className="card__category">{item.category.name}</p>
+      <p className="card__category">
+        {item.category && item.category.name ? item.category.name : ""}
+      </p>
       <h2 className="card__title">{item.title}</h2>
       <p className="card__description">{item.description}</p>
       <p className="card__price">${item.price}</p>
@@ -18,4 +27,4 @@ const Card = (item: ProductItem) => {
   );
 };
 
-export default Card;
+export default CardItem;
