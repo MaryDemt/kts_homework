9191581e79eb48af02903312c0367084cdca17d6
mshevtsova <mary.shevtcova@yandex.ru>
2023-03-01 09:21:18 +0300

    add fixes by code-review

--- cra/src/App/components/Header/Header.tsx
+++ cra/src/App/components/Header/Header.tsx
@@ -1,21 +1,27 @@
 import { BagIcon } from "@components/icons/bag-2";
 import { MainLogo } from "@components/icons/header_logo";
 import { UserIcon } from "@components/icons/user";
+import { Link } from "react-router-dom";
 
 import styles from "./Header.module.scss";
 const Header = () => {
   return (
     <header className={styles.header}>
       <MainLogo />
-      <div className={styles.header__tabs}>
-        <p
-          className={`${styles["header__tabs-item"]} ${styles["header__tabs-item_active"]}`}
+      <nav className={styles.header__tabs}>
+        <Link
+          to="/"
+          className={`${styles["header__item"]} ${styles["header__item_active"]}`}
         >
           Products
-        </p>
-        <p className={styles["header__tabs-item"]}>Categories</p>
-        <p className={styles["header__tabs-item"]}>About Us</p>
-      </div>
+        </Link>
+        <Link to="/categories" className={styles["header__item"]}>
+          Categories
+        </Link>
+        <Link to="/about" className={styles["header__item"]}>
+          About Us
+        </Link>
+      </nav>
       <div className={styles.header__icons}>
         <BagIcon />
         <UserIcon />
